6023f674c44885dcf37e20fafa363661f3d21a34
Soumithira <soumithiravelumani@gmail.com>
2026-04-27 21:41:55 +0530

    Time: 5 ms (5.15%) | Memory: 44.5 MB (6.41%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String r="";
+        int i=0;
+        while(k>0 && i<s.length()){
+            if(s.charAt(i)==' ')
+            k--;
+            if(k>0){
+            r+=s.charAt(i);}
+            i++;
+        }
+        return r;
+    }
+}
