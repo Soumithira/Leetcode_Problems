b883895898d6f639cf857117856f01a0dd05b59e
Soumithira <soumithiravelumani@gmail.com>
2026-05-23 22:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (16.75%) - LeetSync

---
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countDigits(int num) {
+      int t=num;
+      int c=0;
+      while(t>0){
+        int r=t%10;
+        if(num%r==0)  c++;
+        t/=10;
+      }  
+      return c;
+    }
+}
